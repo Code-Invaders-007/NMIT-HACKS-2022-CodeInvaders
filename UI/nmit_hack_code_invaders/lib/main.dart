61899987bd4268e0ac3c9cc6f80ada7e711a851a
Chetan-Managavi <chetanmanagavi@gmail.com>
2022-04-29 15:38:26 +0530

    Firebase Initialised

--- UI/nmit_hack_code_invaders/lib/main.dart
+++ UI/nmit_hack_code_invaders/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+
+
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
   runApp(const MyApp());
 }
 
@@ -36,11 +44,14 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        
 
-    );
+  
+    return Text("HIGH");
+       
+      
+      
   }
 }
