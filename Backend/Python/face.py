b9dd6b76bb2e4a96b3d60d13d779c88829fdf3ff
maruthi-syn-ack <maruthi.syn.ack@gmail.com>
2022-04-30 02:35:30 +0530

    implemented cleaner version of face-recognition

--- Backend/Python/face.py
+++ Backend/Python/face.py
@@ -4,9 +4,10 @@ import face_recognition
 import os
 from datetime import datetime
 import time
+import firebase
 # from PIL import ImageGrab
 
-path = 'refimages'
+path = 'Backend/python1/refimages'
 images = []
 classNames = []
 myList = os.listdir(path)
@@ -20,74 +21,58 @@ print(classNames)
 
 def findEncodings(images):
     encodeList = []
-
-
-
     for img in images:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
-    # print(encodeList)
-
+    
     return encodeList
 
 ymd = time.strftime("%Y%m%d")
-ext = ".csv"
-ymd = ymd+ext
-print(ymd)
 
-def markAttendance(name):
-    with open(ymd, 'a+') as f:
-        f.writelines("Name,Time\n")
-        f.close()
-    with open(ymd, 'r+') as f:
-        myDataList = f.readlines()
-        print(myDataList)
-        
-        
-        nameList = []
-        for line in myDataList:
-            entry = line.split(' ,')
-            nameList.append(entry[0])
-            
-            if name not in nameList:
-                print(nameList)
-                now = datetime.now()
-                dtString = now.strftime('%H:%M:%S')
-                f.writelines(f'\n{name},{dtString}')
+
 
 
 
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 
-while True:
-    success, img = cap.read()
-# img = captureScreen()
-    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+def logic_face():
+   # while True:
+#    for a in range(1,2):
+       # success, img = cap.read()
+    # img = captureScreen()
+        img = cv2.imread('Backend/python1/Test4.jpeg')
+        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-    facesCurFrame = face_recognition.face_locations(imgS)
-    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+        facesCurFrame = face_recognition.face_locations(imgS)
+        encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
-    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-# print(faceDis)
-        matchIndex = np.argmin(faceDis)
+        for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+    # print(faceDis)
+            matchIndex = np.argmin(faceDis)
 
-        if matches[matchIndex]:
-            name = classNames[matchIndex].upper()
-# print(name)
-            y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            markAttendance(name)
+            if matches[matchIndex]:
+                name = classNames[matchIndex].upper()
+    # print(name)
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                
             
 
-    cv2.imshow('Webcam', img)
-    cv2.waitKey(1)
+        cv2.imshow('test image', img)
+        cv2.waitKey(0)
+        # print(faceDis+" - type - "+type(faceDis))
+        # print(matchIndex+" - type - "+type(matchIndex))
+
+
+
+logic_face()
--- Backend/Python/firebase.py
+++ Backend/Python/firebase.py
@@ -1,3 +1,4 @@
+# from Backend/python1/main.py
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -6,11 +7,25 @@ cred = credentials.Certificate("Backend/Python/face-recog-attendance-007-firebas
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-usn = input("Enter USN")
-name_stu = input("Enter your name")
-doc_ref = db.collection(u'students').document(usn)
-doc_ref.set({
-    u'first': name_stu,
-    u'last': u'Lovelace',
-    u'born': 1815
-})
+
+name = 'Abhishek'
+usn = '1SI20AD001'
+facedata =    [1,2,3,4,5,6,]
+
+def write_data( facedata ):
+
+        data = {
+        # data list in json
+
+        "name" :name
+        }
+
+        dept_ref = db.collection('department').document('ai')
+        year_ref = dept_ref.collection('Year').document('1')
+        sect_ref = year_ref.collection('Section').document('A')
+        sect_ref.collection('student').document(usn).set(data,merge=True)
+        print("data Entered")
+        
+        
+
+write_data(facedata)
